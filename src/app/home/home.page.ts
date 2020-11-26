8b44c177d3c2e5147e4d219d7c065ea722e138d1
Nestor RV <nestor.regalado1453@alumnos.udg.mx>
2020-11-26 13:09:49 -0600

    Mostrando peliculas en un ion-card

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../api/peliculas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  peliculas = [/*
+    {
+      titulo: "MAtrix",
+      ano: 1999,
+      director:"Hermanas Wachosky",
+      imagen:"https://cdn.shopify.com/s/files/1/1416/8662/products/the_matrix_1999_bus_stop_original_film_art_1200x.jpg?v=1593122645"
+    },
+    {
+      titulo: "28 Days Later",
+      ano: 2003,
+      director:"Dany Boyle",
+      imagen:"https://images-na.ssl-images-amazon.com/images/I/51iVSo1II5L._AC_SY450_.jpg"
+    }
+  */];
 
-  constructor() {}
+  constructor(private peliculasService: PeliculasService) {}
+
+  ngOnInit(){
+    this.peliculasService.peliculas.subscribe(peliculas =>{
+      this.peliculas = peliculas;
+    });
+    //this.peliculasService.getPeliculas();
+  }
 
 }
